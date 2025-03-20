887cf036aa5c1a0c1b02e11c5ad9ca1c6aefb481
MiyakoOyadomari <dc@MiyakoOyadomarinoMacBook-Pro.local>
2025-03-20 18:50:56 +0900

    logo and metadata

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,7 +2,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <p className="text-red-500 font-bold">Hello Notion Clone</p>;
+      <p className="h-full bg-amber-200 text-red-500 font-bold">
+        Hello Notion Clone
+      </p>
       <Button>Click me</Button>
     </>
   );
